3bf1b3cd9f064a30e973490b5d4710965fa6171c
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-02-05 00:35:00 +0400

    Fill 07_loops.php with code

--- 07_loops.php
+++ 07_loops.php
@@ -1,12 +1,55 @@
 <?php
 
 // while
+while (true) { // Infinite loop: DON'T run this
+    // Do something constantly
+}
+
+$counter = 0; // When counter is 10??
+while ($counter < 10) {
+    echo "Printing counter: $counter<br>";
+    // if ($counter > 5) break; // BREAK
+    $counter++;
+}
+
+$runLoop = true;
+$counter = 0;
+while ($runLoop) {
+    echo "Printing counter: $counter<br>";
+    $counter++;
+    if ($counter > 10) {
+        $runLoop = false;
+    }
+}
 
 // do - while
+$counter = 0; // When counter is 10
+do {
+    // Do some code right here
+    $counter++;
+} while ($counter < 10);
 
 // for
-// foreach
-// iterating over elements of array, counting sum of elements
+for ($i = 0; $i < 10; $i++) {
+    echo "Printing counter: $i<br>";
+}
 
-// Iterate
-// Over associative array.
+// foreach
+$fruits = ["Banana", "Apple", "Orange"];
+foreach ($fruits as $i => $fruit) {
+    echo $i . ' ' . $fruit . '<br>';
+}
+
+// Iterate Over associative array.
+$person = [
+    'name' => 'Brad',
+    'surname' => 'Traversy',
+    'age' => 30,
+    'hobbies' => ['Tennis', 'Video Games'],
+];
+foreach ($person as $key => $value) {
+    if ($key === 'hobbies') {
+        break;
+    }
+    echo $key . ' ' . $value . '<br>';
+}
