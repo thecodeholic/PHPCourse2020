e5047c7e59683a28617f58ab758c2b5d7d5422be
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-02-05 00:50:36 +0400

    Fill 08_functions.php

--- 08_functions.php
+++ 08_functions.php
@@ -1,5 +1,36 @@
 <?php
 
-// Create sum of two functions
-// Create function to sum all numbers using ...$nums
-// Arrow functions
+// 1. Function which prints "Hello I am TheCodeholic"
+function hello()
+{
+    echo 'Hello I am TheCodeholic<br>';
+}
+
+hello(); // 2
+hello(); // 3
+hello(); // 4
+
+// 5. Create sum of two functions
+function sum($a, $b)
+{
+    echo ($a + $b) . '<br>'; // 8. return sum
+}
+
+sum(4,5); // 6, 9 [echo]
+sum(9,10); // 7, 10 [echo]
+
+//// 11. Create function to sum all numbers using ...$nums
+//function sum(...$nums)
+//{
+//    $sum = 0;
+//    foreach ($nums as $num) $sum += $num;
+//    return $sum;
+//}
+//
+//// 13 Arrow functions
+//function sum(...$nums)
+//{
+//    return array_reduce($nums, fn($coll, $n) => $coll + $n);
+//}
+//
+//echo sum(1, 2, 3, 4, 6); // 12
