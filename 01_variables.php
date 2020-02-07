85e37ca83f9c21ba4166cf2c5b31fd3e34673d3d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-02-07 09:00:46 +0400

    Add constants into variables

--- 01_variables.php
+++ 01_variables.php
@@ -49,3 +49,18 @@ is_string($name); // false
 is_int($age); // true
 is_bool($isMale); // true
 is_double($height); // true
+
+// 9. Check if variable is defined
+var_dump(isset($name));
+var_dump(isset($name2));
+echo '<br>';
+
+// 10. Constants
+define('PI', 3.14);
+echo PI.'<br>';
+var_dump(defined('PI')); // defined(PI2)
+echo '<br>';
+
+// 12. Using PHP built-in constants
+echo SORT_ASC.'<br>';
+echo PHP_INT_MAX.'<br>';
