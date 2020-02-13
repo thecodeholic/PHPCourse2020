f343779b7dc6959826a190f3228c3359ba883287
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-02-13 10:03:00 +0400

    Write code for include and require

---
+++ 11_include_require/01_website_skeleton/partials/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: TheCodeholic
+ * Date: 2/10/2020
+ * Time: 8:39 AM
+ */
+?>
+
+<footer>
+    Copyright &copy; <?php echo $companyName ?>
+</footer>
+</body>
+</html>
+
---
+++ 11_include_require/01_website_skeleton/partials/weather.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: TheCodeholic
+ * Date: 2/13/2020
+ * Time: 9:25 AM
+ */
+?>
+
+<h3>Georgia, Tbilisi 5&#8451;</h3>
