e832e1dfdacf209adc1e0a13cb28c13f83202728
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-02-18 10:09:05 +0400

    Cleanup code for include and require

--- 11_include_require/02_functions/index.php
+++ 11_include_require/02_functions/index.php
@@ -4,8 +4,3 @@
  * Date: 2/13/2020
  * Time: 9:31 AM
  */
-// We need to have here require not include
-require_once 'math.php';
-
-echo add(4, 5).'<br>';
-echo subtract(9, 4);
--- 11_include_require/02_functions/math.php
+++ 11_include_require/02_functions/math.php
@@ -4,13 +4,3 @@
  * Date: 2/13/2020
  * Time: 9:31 AM
  */
-
-function add($a, $b)
-{
-    return $a + $b;
-}
-
-function subtract($a, $b)
-{
-    return $a - $b;
-}
