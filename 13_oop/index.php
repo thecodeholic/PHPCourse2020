98c325fdd94583ba4feced7408c49830a56f5e1d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-02-18 10:11:53 +0400

    Cleanup OOP folder

--- 13_oop/index.php
+++ 13_oop/index.php
@@ -1,21 +1,9 @@
 <?php
 
-// 1. What is class and instance
+// What is class and instance
 
-// 2. Create Person class in Person.php
-require_once './Person.php';
-require_once './Singer.php';
+// Create Person class in Person.php
 
-// 4. Create instance of Person
-$p = new Person("Zura", 28, 100); // 8. Add arguments to constructor
-$p->name = 'Zura';
-//$p->age = 28; // Can not access protected and private properties
-//$p->salary = 100; // Can not access protected and private properties
+// Create instance of Person
 
-// 6. Using setter and getter
-$p->setAge(28);
-echo $p->getAge() . '<br>';
-
-
-$s = new Singer("Rihanna", 29, 200);
-echo $s->getAge();
+// Using setter and getter
